513b8eb289653a943ce8ffa3690ea7fc9b3ebe45
AliTaleshi <alitys083@gmail.com>
2025-04-22 15:54:36 +0330

    Add Main class

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import broker.FileMessageBroker;
+import consumer.Consumer;
+import producer.Producer;
+
+public class Main {
+    public static void main(String[] args) {
+        FileMessageBroker broker = new FileMessageBroker();
+
+        // Start Consumers
+        for (int i = 0; i < 3; i++) {
+            String consumerId = "consumer-" + i;
+            Thread consumerThread = new Thread(new Consumer(consumerId, broker), consumerId);
+            consumerThread.start();
+        }
+
+        // Start Producers
+        for (int i = 0; i < 2; i++) {
+            String producerName = "Producer-" + i;
+            Thread producerThread = new Thread(new Producer(broker, producerName));
+            producerThread.start();
+        }
+    }
+}
